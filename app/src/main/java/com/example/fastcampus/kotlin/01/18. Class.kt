bc5645ab8c4843bc5f8bcb194b7c75472b153c2e
DDing77 <smg76262585@gmail.com>
2021-08-01 23:40:23 +0900

    Update 18. Class.kt

--- app/src/main/java/com/example/fastcampus/kotlin/01/18. Class.kt
+++ app/src/main/java/com/example/fastcampus/kotlin/01/18. Class.kt
@@ -20,13 +20,22 @@ fun main(array: Array<String>) {
 
     // 클래스(설명서)를 통해서 실체를 만드는 방법
     // -> 인스턴스화 -> 인스턴스(객체)
-    Car ("v8 engine", "big")
+    Car("v8 engine", "big")
     val bigCar = Car("v8 engine", "big")
 
     // 우리가 만든 클래스(설명서)는 자료형이 된다
-    val bigCar1: Car = Car("v8 engine","big")
+    val bigCar1: Car = Car("v8 engine", "big")
+    val superCar: SuperCar = SuperCar("good engine", "big", "white")
 
-    val superCar: SuperCar = SuperCar("good engine","big","white")
+    val runableCar : RunableCar = RunableCar("simple engine", "short body")
+    runableCar.ride()
+    runableCar.navi("부산")
+    runableCar.drive()
+
+    // 인스턴스의 멤버 변수에 접근 하는 방법
+    val runableCar2: RunableCar2 = RunableCar2("nice engine", "long body")
+    println(runableCar2.body)
+    println(runableCar2.engine)
 }
 
 // 클래스 만드는 방법(1)
@@ -50,9 +59,76 @@ class SuperCar {
     }
 }
 
+//  클래스(설명서) 만드는 방법(3) -> 1번 방법의 확장
+class Car1 constructor(engine: String, body: String) {
+    var door: String = ""
+
+    // 생성자
+    constructor(engine: String, body: String, door: String) : this(engine, body) {
+        this.door = door
+    }
+}
+
+// 클래스(설명서) 만드는 방법(4) -> 2번 방법의 확장
+class Car2 {
+    var engine: String = ""
+    var body: String = ""
+    var door: String = ""
+
+    constructor(engine: String, body: String) {
+        this.engine = engine
+        this.body = body
+    }
+
+    constructor(engine: String, body: String, door: String) {
+        this.engine = engine
+        this.body = body
+        this.door = door
+    }
+}
 
 
+class RunableCar(engine: String, body: String) {
 
+    fun ride() {
+        println("탑승 하였습니다")
+    }
+
+    fun drive() {
+        println("달립니다 !")
+    }
+
+    fun navi(destination : String) {
+        println("$destination 으로 목적지가 설정되었습니다")
+    }
+}
+
+class RunableCar2 {
+    var engine: String
+    var body: String
+
+    constructor(engine: String, body: String) {
+        this.engine = engine
+        this.body = body
+    }
+
+    // 인스턴스화가 될 때 가장 먼저 호출된다
+    init {
+        // 초기셋팅을 할 때 유용하다
+        println("RunalbeCar2가 만들어 졌습니다")
+    }
+    fun ride() {
+        println("탑승 하였습니다")
+    }
+
+    fun drive() {
+        println("달립니다 !")
+    }
+
+    fun navi(destination : String) {
+        println("$destination 으로 목적지가 설정되었습니다")
+    }
+}
 
 
 
