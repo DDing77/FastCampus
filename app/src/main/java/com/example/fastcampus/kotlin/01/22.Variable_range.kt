cc680859d5054638d5412563530f7aed5ebfb38f
DDing77 <smg76262585@gmail.com>
2021-08-05 21:16:22 +0900

    Create 22.Variable_range.kt

---
+++ app/src/main/java/com/example/fastcampus/kotlin/01/22.Variable_range.kt
@@ -0,0 +1,37 @@
+package com.example.fastcampus.kotlin.`01`
+
+
+// 변수의 접근 범위
+// 1. 전역 변수
+// 2. 지역 변수
+
+// 전역 변수 설정은 작게헤야 좋다
+
+var number100: Int = 10 // 전역 변수
+
+fun main(args: Array<String>) {
+    println(number100)
+
+    val test = Test("홍길동")
+    test.testFun()
+    test.name
+    println(number100)
+}
+
+class Test(var name : String ) { // 지역변수
+
+    fun testFun() {
+        var birth: String = "2000/3/1"
+        name = "홍길동"
+        number100 = 100
+        fun testFun2(){
+            var gender: String = "male"
+        }
+    }
+
+    fun testFun2(){
+        name
+        birth
+    }
+
+}
