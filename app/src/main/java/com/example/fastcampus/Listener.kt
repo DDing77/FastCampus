0098412195e879df0ac6d79d3c5397071f9af979
DDing77 <smg76262585@gmail.com>
2021-08-26 23:38:29 +0900

    Get XML file

---
+++ app/src/main/java/com/example/fastcampus/Listener.kt
@@ -0,0 +1,43 @@
+package com.example.fastcampus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android. synthetic.main.activity_listener.*
+import android.widget.TextView
+
+class Listener : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listener)
+
+        // 뷰를 activity로 가져오는 방법
+        // 1> 직접 찾아서 가져온다
+//        val textView : TextView = findViewById(R.id.hello)
+
+        // 2> xml을 import해서 가져온다
+//            hello.
+
+        // 익명함수
+        // 1 -> 람다 방식
+        hello.setOnClickListener {
+            Log.d("click", "Click")
+        }
+
+        // 2 -> 익명 함수 방식
+        hello.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Log.d("click", "Click")
+            }
+        })
+
+        // 3 -> 이름이 필요한 경우
+        val click = object: View.OnClickListener{
+            override fun onClick(v: View?) {
+
+            }
+        }
+        hello.setOnClickListener(click)
+    }
+}
