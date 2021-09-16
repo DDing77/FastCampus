42a653a8fabcdbe7a5478126b023d3ba2ce97b92
DDing77 <smg76262585@gmail.com>
2021-09-16 21:15:41 +0900

    Fragment02

--- app/src/main/java/com/example/fastcampus/FragmentActivity.kt
+++ app/src/main/java/com/example/fastcampus/FragmentActivity.kt
@@ -3,36 +3,54 @@ package com.example.fastcampus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
-        Log.d("life_cycle","onCreate")
+        Log.d("life_cycle", "onCreate")
+
+        button.setOnClickListener {
+            // 프라그먼트를 동적으로 작동하는 방법
+            val fragmentOne: FragmentOne = FragmentOne();
+            val fragmentManager: FragmentManager = supportFragmentManager
+
+            //Transaction
+            // 작업의 단위 -> 시작과 끝이 있다
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container,fragmentOne)
+            fragmentTransaction.commit()
+            // 끝을 내는 방법
+            // commit       -> 시간 될 때 해 (좀 더 안정적)
+            // commitnow    -> 지금 당장
+
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("life_cycle","onStart")
+        Log.d("life_cycle", "onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("life_cycle","onResume")
+        Log.d("life_cycle", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("life_cycle","onPause")
+        Log.d("life_cycle", "onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("life_cycle","onStop")
+        Log.d("life_cycle", "onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("life_cycle","onDestroy")
+        Log.d("life_cycle", "onDestroy")
     }
 }
